08512e838b87b27035ffee10c925affa4298c0da
unknown <stephan.do08@gmail.com>
2017-11-28 19:42:07 +0100

    genealogy.pl

---
+++ genealogy.pl
@@ -0,0 +1,23 @@
+parent(adam, john).
+parent(eve, john).
+parent(eve, lisa).
+parent(john, anne).
+parent(john, pat).
+parent(pat, jacob).
+parent(carol, jacob).
+
+isPatParent(X) :- parent(X, pat).
+isChildOfLisa(X) :- parent(lisa, X).
+
+male(adam).
+male(john).
+male(jacob).
+
+female(eve).
+female(lisa).
+female(anne).
+female(pat).
+female(carol).
+
+isGrandparent(X) :- parent(X, Y), parent(Y, pat), parent(Y, anne).
+isGrandparent(X) :- parent(X, Y), parent(Y, jacob).
