d510ac437704763bb72a5fec39ba3e2404c99fdc
unknown <stephan.do08@gmail.com>
2017-11-28 20:08:43 +0100

    genealogy

--- genealogy.pl
+++ genealogy.pl
@@ -12,14 +12,11 @@ isChildOfLisa(X) :- parent(lisa, X).
 male(adam).
 male(john).
 male(jacob).
-
 female(eve).
 female(lisa).
 female(anne).
 female(pat).
 female(carol).
-awdadasdad
+
 isGrandparent(X) :- parent(X, Y), parent(Y, pat), parent(Y, anne).
 isGrandparent(X) :- parent(X, Y), parent(Y, jacob).
-
-
